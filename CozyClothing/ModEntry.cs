5b987f8751db79b473423a3c75469ea8844aa43e
Lauren Stephenson <compscilauren@gmail.com>
2020-01-01 19:23:07 -0600

    Add basic setup code for mod

---
+++ CozyClothing/ModEntry.cs
@@ -0,0 +1,24 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace CozyClothing
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+
+        /// <summary>Raised after the save file is loaded.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            // do stuff
+        }
+    }
+}
