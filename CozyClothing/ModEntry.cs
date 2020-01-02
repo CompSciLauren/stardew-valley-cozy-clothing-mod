f2a9fe822fa50954c4ad8406754d539c234d7291
Lauren Stephenson <compscilauren@gmail.com>
2020-01-02 14:45:13 -0600

    Add Config options for pajama color

--- CozyClothing/ModEntry.cs
+++ CozyClothing/ModEntry.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
@@ -9,6 +8,9 @@ namespace CozyClothing
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
+        /// <summary>The mod configuration from the player.</summary>
+        private ModConfig Config;
+
         private int previousShirt;
         private int previousPantStyle;
         private Color previousPantsColor;
@@ -18,6 +20,7 @@ namespace CozyClothing
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
+            Config = Helper.ReadConfig<ModConfig>();
             Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
         }
 
@@ -26,7 +29,7 @@ namespace CozyClothing
         /// <param name="e">The event data.</param>
         private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
         {
-            ChangeShirtToPajamaShirt();
+            ChangeIntoPajamas();
 
             Helper.Events.Player.Warped += OnWarped;
         }
@@ -46,11 +49,11 @@ namespace CozyClothing
             }
             else if (e.NewLocation is StardewValley.Locations.FarmHouse)
             {
-                ChangeShirtToPajamaShirt();
+                ChangeIntoPajamas();
             }
         }
 
-        private void ChangeShirtToPajamaShirt()
+        private void ChangeIntoPajamas()
         {
             // save current clothes to change back into later
             previousShirt = Game1.player.shirt.Value;
@@ -59,10 +62,37 @@ namespace CozyClothing
             previousShoeColor = Game1.player.shoes.Value;
 
             // change current clothes to be pajamas
-            Game1.player.changeShirt(9);
             Game1.player.changePantStyle(0);
-            Game1.player.changePants(Color.DarkTurquoise);
-            Game1.player.changeShoeColor(6);
+
+            switch (Config.PajamaColor)
+            {
+                case "Pink":
+                    Game1.player.changeShirt(36);
+                    Game1.player.changePants(Color.PaleVioletRed);
+                    Game1.player.changeShoeColor(4);
+                    break;
+                case "Purple":
+                    Game1.player.changeShirt(40);
+                    Game1.player.changePants(Color.MediumPurple);
+                    Game1.player.changeShoeColor(4);
+                    break;
+                case "Green":
+                    Game1.player.changeShirt(96);
+                    Game1.player.changePants(Color.LimeGreen);
+                    Game1.player.changeShoeColor(4);
+                    break;
+                case "Water-Blue":
+                    Game1.player.changeShirt(105);
+                    Game1.player.changePants(Color.RoyalBlue);
+                    Game1.player.changeShoeColor(4);
+                    break;
+                case "Blue":
+                default:
+                    Game1.player.changeShirt(9);
+                    Game1.player.changePants(Color.DarkTurquoise);
+                    Game1.player.changeShoeColor(6);
+                    break;
+            }
         }
     }
 }
